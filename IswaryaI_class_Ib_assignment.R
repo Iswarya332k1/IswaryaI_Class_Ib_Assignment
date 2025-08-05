585b5ecb9976fa0f4879934e16c78c6c3350e747
ISWARYA I <ishuiyamperumal30@gmail.com>
2025-08-05 05:42:05 -0700

    Upload my RData file

---
+++ IswaryaI_class_Ib_assignment.R
@@ -0,0 +1,38 @@
+Load the dataset
+patient_info <- read.csv("patient_info.csv")
+
+# Inspect the structure
+str(patient_info)
+
+# Identify variables with incorrect types
+#From str() output, we see:
+# - patient_id, gender, diagnosis, smoker are character (chr)
+# - age is integer (correct)
+# - bmi is numeric (correct)
+
+# Convert variables into appropriate types
+# Convert smoker to factor (categorical)
+patient_info$smoker <- factor(patient_info$smoker)
+
+# Convert diagnosis to factor
+patient_info$diagnosis <- factor(patient_info$gender)
+
+# Convert gender to factor
+patient_info$gender <- factor(patient_info$gender)
+
+# Create new binary smoking status variable
+patient_info$smoking_status <- ifalse(patient_info$smoker  ==  "Yes", 1, 0)
+
+# Verify the changes
+str(patient_info)
+
+# View the first few rows to see the new variable
+head(patient_info)
+
+# Summary Statistics
+summary(patient_info)
+
+
+
+
+
